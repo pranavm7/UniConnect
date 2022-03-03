0d4375a66963ce6e5c9857a2b4c70d4016bddb61
billapp99 <67777160+billapp99@users.noreply.github.com>
2022-03-03 17:20:12 -0500

    Unit test for post toString passes

--- UniConnectApp/app/src/main/java/com/example/uniconnect/dto/Post.kt
+++ UniConnectApp/app/src/main/java/com/example/uniconnect/dto/Post.kt
@@ -1,5 +1,8 @@
 package com.example.uniconnect.dto
 
 data class Post(var title: String, var description: String) {
+    override fun toString(): String {
+        return "$title - $description"
+    }
 
 }
--- UniConnectApp/app/src/test/java/com/example/uniconnect/UniConnectUnitTest.kt
+++ UniConnectApp/app/src/test/java/com/example/uniconnect/UniConnectUnitTest.kt
@@ -18,9 +18,9 @@ class UniConnectUnitTest {
         assertTrue(post.description.equals("In Corbet Auditorium"))
     }
     @Test
-    fun `Given a post DTO when  title is CCM Concert and description is In Corbet Auditorium then output is CCM Concert : In Corbet Auditorium`(){
+    fun `Given a post DTO when  title is CCM Concert and description is In Corbet Auditorium then output is CCM Concert - In Corbet Auditorium`(){
         var post = Post("CCM Concert", "In Corbet Auditorium")
-        assertTrue(post.toString().equals("CCM Concert : In Corbet Auditorium"))
+        assertTrue(post.toString().equals("CCM Concert - In Corbet Auditorium"))
     }
 
 }
