4e5c8274879cd825599e6a0aeeef9473af648b81
MrPandey2k <66745368+MrPandey2k@users.noreply.github.com>
2022-03-05 12:09:06 -0500

    Filled out live data unit test/ Dependencies added in gradle

--- UniConnectApp/app/src/test/java/com/example/uniconnect/UniConnectUnitTest.kt
+++ UniConnectApp/app/src/test/java/com/example/uniconnect/UniConnectUnitTest.kt
@@ -1,9 +1,16 @@
 package com.example.uniconnect
 
+import androidx.lifecycle.Observer
 import com.example.uniconnect.dto.Post
+import com.example.uniconnect.dto.University
 import org.junit.Test
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
 
 import org.junit.Assert.*
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,6 +18,12 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class UniConnectUnitTest {
+
+    lateinit var mvm : MainViewModel
+
+    @MockK
+    lateinit var mockUniService : UniversityService
+
     @Test
     fun `Given a post DTO when title is CCM Concert and description is In Corbet Auditorium `(){
         var post = Post("CCM Concert", "In Corbet Auditorium")
@@ -30,14 +43,34 @@ class UniConnectUnitTest {
     }
 
     private fun viewModelInitializedWithMockData() {
+        val universities = ArrayList<University>()
+        universities.add(University("University of Cincinnati", "United States", "US"))
+        universities.add(University("Xavier University", "United States", "US"))
+        universities.add(University("University of Cambridge", "United Kingdom", "GB"))
+
+        coEvery{ mockUniService.fetchUniversities()} returns universities
+        mvm.universityService = mockUniService
     }
 
     private fun jsonDataIsParasedAndRead() {
-        TODO("Not yet implemented")
+        mvm.fetchUniversities()
     }
 
     private fun thenResultsShouldContainUOfCinci() {
-        TODO("Not yet implemented")
+        var allUni : List<University>? = ArrayList<University>()
+        val latch = CountDownLatch(1);
+        val observer = object : Observer<List<University>> {
+            override fun onChanged(x: List<University>?) {
+                allUni = x
+                latch.countDown()
+                mvm.universities.removeObserver(this)
+            }
+        }
+        mvm.universities.observeForever(observer)
+
+        latch.await(1, TimeUnit.SECONDS)
+        assertNotNull(allUni)
+        assertTrue(allUni!!.contains(University("University of Cincinnati", "United States", "US")))
     }
 
 }
