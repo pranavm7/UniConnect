b775a8d072757258bc7f521da78cc6f41fbc5b02
Bipal Goyal <goyalbl@mail.uc.edu>
2022-03-05 20:23:12 -0500

    Integration test to bring university JSON data

--- UniConnectApp/app/src/test/java/com/example/uniconnect/UniConnectUnitTest.kt
+++ UniConnectApp/app/src/test/java/com/example/uniconnect/UniConnectUnitTest.kt
@@ -1,14 +1,21 @@
 package com.example.uniconnect
 
-import androidx.lifecycle.Observer
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.uniconnect.dto.Post
 import com.example.uniconnect.dto.University
 import org.junit.Test
-import io.mockk.MockKAnnotations
-import io.mockk.coEvery
-import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.rules.TestRule
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
@@ -24,6 +31,24 @@ class UniConnectUnitTest {
     //@MockK
     //lateinit var mockUniService : UniversityService
 
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+    lateinit var universityService: IUniversityService
+    var allUniversities : List<University>? = ArrayList<University>()
+
+    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+
+    @Before
+    fun populateUniversities() {
+        Dispatchers.setMain(mainThreadSurrogate)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain() // reset the main dispatcher to the original Main dispatcher
+        mainThreadSurrogate.close()
+    }
+
     @Test
     fun `Given a post DTO when title is CCM Concert and description is In Corbet Auditorium `(){
         var post = Post("CCM Concert", "In Corbet Auditorium")
@@ -32,18 +57,39 @@ class UniConnectUnitTest {
     }
     @Test
     fun `Given a post DTO when  title is CCM Concert and description is In Corbet Auditorium then output is CCM Concert - In Corbet Auditorium`(){
-        var post = Post("CCM Concert", "In Corbet Auditorium")
+        val post = Post("CCM Concert", "In Corbet Auditorium")
         assertTrue(post.toString().equals("CCM Concert - In Corbet Auditorium"))
     }
 
     @Test
     fun `Given a University DTO when name is MaryWood University and country is United States`(){
-        var university = University("MaryWood University", "United States", "US")
+        val university = University("MaryWood University", "United States", "US")
         assertTrue(university.name.equals("MaryWood University"))
         assertTrue(university.country.equals("United States"))
     }
 
+    @Test
+    fun `Given service connects to University JSON stream when data are read and parsed then university collection should be greater than zero`() =
+        runTest {
+        launch(Dispatchers.Main) {
+            givenUniversityServiceIsInitialized()
+            whenServiceDataAreReadAndParsed()
+            thenTheUniversityCollectionSizeShouldBeGreaterThanZero()
+        }
+    }
 
+    private fun givenUniversityServiceIsInitialized() {
+        universityService = UniversityService()
+    }
+
+    private suspend fun whenServiceDataAreReadAndParsed() {
+        allUniversities = universityService.fetchUniversities()
+    }
+
+    private fun thenTheUniversityCollectionSizeShouldBeGreaterThanZero() {
+        assertNotNull(allUniversities)
+        assertTrue(allUniversities!!.isNotEmpty())
+    }
 
     /*@Test
     fun `Given a viewModel with live data when populated with univeristies results should show University Of Cincinnati`(){
