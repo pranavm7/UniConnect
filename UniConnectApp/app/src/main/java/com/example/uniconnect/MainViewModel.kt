b6051c50b4dd87498e8fe3de17cfc8ae52401a31
Bipal Goyal <goyalbl@mail.uc.edu>
2022-03-23 19:24:32 -0400

    save post to Firebase

--- UniConnectApp/app/src/main/java/com/example/uniconnect/MainViewModel.kt
+++ UniConnectApp/app/src/main/java/com/example/uniconnect/MainViewModel.kt
@@ -1,10 +1,31 @@
 package com.example.uniconnect
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.uniconnect.dto.Post
 import com.example.uniconnect.service.IUniversityService
 import com.example.uniconnect.service.UniversityService
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 
 class MainViewModel(var universityService: IUniversityService = UniversityService()) : ViewModel() {
     private lateinit var firestore : FirebaseFirestore
+
+    init {
+        firestore = FirebaseFirestore.getInstance()
+        firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
+    }
+    fun savePost (post: Post) {
+        val doc = if (post.postId == null || post.postId.isEmpty()) {
+            // insert
+            firestore.collection("posts").document()
+        } else {
+            // update
+            firestore.collection("posts").document(post.postId)
+        }
+        post.postId = doc.id
+        val handle = doc.set(post)
+        handle.addOnSuccessListener { Log.d("Firebase", "document saved") }
+        handle.addOnFailureListener { Log.d("Firebase", "Error saving document $it")}
+    }
 }
--- UniConnectApp/app/src/test/java/com/example/uniconnect/UniConnectUnitTest.kt
+++ UniConnectApp/app/src/test/java/com/example/uniconnect/UniConnectUnitTest.kt
@@ -53,13 +53,13 @@ class UniConnectUnitTest {
 
     @Test
     fun `Given a post DTO when title is CCM Concert and description is In Corbet Auditorium `(){
-        val post = Post("CCM Concert", "In Corbet Auditorium")
+        val post = Post("","CCM Concert", "In Corbet Auditorium")
         assertTrue(post.title.equals("CCM Concert"))
         assertTrue(post.description.equals("In Corbet Auditorium"))
     }
     @Test
     fun `Given a post DTO when  title is CCM Concert and description is In Corbet Auditorium then output is CCM Concert - In Corbet Auditorium`(){
-        val post = Post("CCM Concert", "In Corbet Auditorium")
+        val post = Post("","CCM Concert", "In Corbet Auditorium")
         assertTrue(post.toString().equals("CCM Concert - In Corbet Auditorium"))
     }
 
