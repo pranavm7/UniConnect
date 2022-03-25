39e7c6eaa7d486adb0d728ebd8e341ac56956bba
Bipal Goyal <goyalbl@mail.uc.edu>
2022-03-25 16:57:22 -0400

    retrieve posts from Firebase

--- UniConnectApp/app/src/main/java/com/example/uniconnect/AppModule.kt
+++ UniConnectApp/app/src/main/java/com/example/uniconnect/AppModule.kt
@@ -2,8 +2,10 @@ package com.example.uniconnect
 
 import com.example.uniconnect.service.IUniversityService
 import com.example.uniconnect.service.UniversityService
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
+    viewModel { MainViewModel(get())}
     single<IUniversityService> { UniversityService() }
 }
--- UniConnectApp/app/src/main/java/com/example/uniconnect/MainViewModel.kt
+++ UniConnectApp/app/src/main/java/com/example/uniconnect/MainViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.uniconnect
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.uniconnect.dto.Post
 import com.example.uniconnect.service.IUniversityService
@@ -9,14 +10,44 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
 
 class MainViewModel(var universityService: IUniversityService = UniversityService()) : ViewModel() {
-    private lateinit var firestore : FirebaseFirestore
+    val posts: MutableLiveData<List<Post>> = MutableLiveData<List<Post>>()
+
+    private var firestore : FirebaseFirestore = FirebaseFirestore.getInstance()
 
     init {
-        firestore = FirebaseFirestore.getInstance()
         firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
+        listenToPost()
     }
+
+    fun listenToPost() {
+        firestore.collection("posts").addSnapshotListener {
+                snapshot, error ->
+            // see of we received an error
+            if (error != null) {
+                Log.w("listen failed.", error)
+                return@addSnapshotListener
+            }
+            // if we reached this point, there was not an error, and we have data.
+            snapshot?.let {
+                val allPosts = ArrayList<Post>()
+
+                /*allPosts.add(Post(NEW_SPECIMEN))*/
+
+                val documents = snapshot.documents
+                documents.forEach {
+                    val post = it.toObject(Post::class.java)
+                    post?.let {
+                        allPosts.add(post)
+                    }
+                }
+                // we have a populated collection of posts.
+                posts.value = allPosts
+            }
+        }
+    }
+
     fun savePost (post: Post) {
-        val doc = if (post.postId == null || post.postId.isEmpty()) {
+        val doc = if (post.postId == "" || post.postId.isEmpty()) {
             // insert
             firestore.collection("posts").document()
         } else {
