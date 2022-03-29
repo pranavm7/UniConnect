5cbd60e4fc3e902c66e574ff0417b34a532054da
billapp99 <67777160+billapp99@users.noreply.github.com>
2022-03-29 12:12:25 -0400

    Added title and description for when a user clicks in text field

--- UniConnectApp/app/src/main/java/com/example/uniconnect/MainActivity.kt
+++ UniConnectApp/app/src/main/java/com/example/uniconnect/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.uniconnect
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.uniconnect.ui.theme.UniConnectTheme
 
@@ -30,6 +33,16 @@ class MainActivity : ComponentActivity() {
 fun PostDetails(name: String) {
     var title by remember {mutableStateOf("")}
     var description by remember { mutableStateOf("")}
+    Column{
+        OutlinedTextField(
+            value = title,
+            onValueChange = {title = it},
+            label = { Text(stringResource(R.string.title))}
+        )
+
+
+
+    }
 }
 
 @Preview(showBackground = true)
