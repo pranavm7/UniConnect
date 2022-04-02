0273cdd726035f3191293d1bd5d6f23a80b7cd6e
Edward Bender <59852058+bendered2332@users.noreply.github.com>
2022-04-02 15:45:26 -0400

    SavePost function correctly added to Button onClick event

--- UniConnectApp/app/src/main/java/com/example/uniconnect/MainActivity.kt
+++ UniConnectApp/app/src/main/java/com/example/uniconnect/MainActivity.kt
@@ -19,8 +19,8 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
     private val viewModel: MainViewModel by viewModel<MainViewModel>()
-    private var inTitle : String = ""
-    private var inDescription : String = ""
+    //private var inTitle : String = ""
+    //private var inDescription : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,43 +33,41 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Composable
-fun PostDetails(name: String) {
-    var title by remember {mutableStateOf("")}
-    var description by remember { mutableStateOf("")}
-    var postID by remember { mutableStateOf("")}
-    val context = LocalContext.current
-    Column{
-        OutlinedTextField(
-            value = title,
-            onValueChange = {title = it},
-            label = { Text(stringResource(R.string.title))}
-        )
-        OutlinedTextField(
-            value = description,
-            onValueChange = {description =it},
-            label = { Text(stringResource(R.string.description))}
-        )
-        Button(
-            onClick = {
-                var post = Post().apply{
-                    title = title
-                    description = description
-                    postID = postID
+    @Composable
+    fun PostDetails(name: String) {
+        var title by remember () {mutableStateOf("")}
+        var description by remember { mutableStateOf("")}
+        //var postID by remember { mutableStateOf("")}
+        val context = LocalContext.current
+        Column{
+            OutlinedTextField(
+                value = title,
+                onValueChange = {title = it},
+                label = { Text(stringResource(R.string.title))}
+            )
+            OutlinedTextField(
+                value = description,
+                onValueChange = {description =it},
+                label = { Text(stringResource(R.string.description))}
+            )
+            Button(
+                onClick = {
+                    var post = Post(title = title, description = description)
+                    
+                    viewModel.savePost(post)
+                    Toast.makeText(context, ", $title, $description", Toast.LENGTH_LONG).show()
                 }
-                //SavePost function goes here 
-                Toast.makeText(context, "$postID, $title, $description", Toast.LENGTH_LONG).show()
-        }
             ){Text(text = "Post")}
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        UniConnectTheme {
+            PostDetails("Android")
+        }
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    UniConnectTheme {
-        PostDetails("Android")
-    }
-}
