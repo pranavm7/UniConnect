38b4bab913df69d1302d8ff33fe5232da563184e
Bipal Goyal <goyalbl@mail.uc.edu>
2022-04-02 21:40:19 -0400

    add firebase authentication

--- UniConnectApp/app/src/main/java/com/example/uniconnect/MainActivity.kt
+++ UniConnectApp/app/src/main/java/com/example/uniconnect/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.uniconnect
 
+import android.app.Activity.RESULT_OK
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,8 +13,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.uniconnect.ui.theme.UniConnectTheme
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : ComponentActivity() {
+
+    private var user: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,6 +32,32 @@ class MainActivity : ComponentActivity() {
                     Greeting("Android")
                 }
             }
+        }
+    }
+
+    private fun signOn() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build()
+        )
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+
+    private val signInLauncher =
+        registerForActivityResult(FirebaseAuthUIActivityResultContract())
+        { res -> this.signInResult(res)}
+
+    private fun signInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            user = FirebaseAuth.getInstance().currentUser
+        }
+        else {
+            Log.e("MainActivity.kt", "Error Logging in " + response?.error?.errorCode)
         }
     }
 }
