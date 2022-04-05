82736a63388238a116646b2074f7c5587d6080b0
Chris Brown <46230726+ccbrowndev@users.noreply.github.com>
2022-04-05 15:17:31 -0400

    Move unit tests related to the Post DTO to their own file

--- UniConnectApp/app/src/test/java/com/example/uniconnect/UniConnectUnitTest.kt
+++ UniConnectApp/app/src/test/java/com/example/uniconnect/UniConnectUnitTest.kt
@@ -51,18 +51,7 @@ class UniConnectUnitTest {
         mainThreadSurrogate.close()
     }
 
-    @Test
-    fun `Given a post DTO when title is CCM Concert and description is In Corbet Auditorium `() {
-        val post = Post("", "CCM Concert", "In Corbet Auditorium")
-        assertTrue(post.title.equals("CCM Concert"))
-        assertTrue(post.description.equals("In Corbet Auditorium"))
-    }
 
-    @Test
-    fun `Given a post DTO when  title is CCM Concert and description is In Corbet Auditorium then output is CCM Concert - In Corbet Auditorium`() {
-        val post = Post("", "CCM Concert", "In Corbet Auditorium")
-        assertTrue(post.toString().equals("CCM Concert - In Corbet Auditorium"))
-    }
 
     @Test
     fun `Given a University DTO when name is MaryWood University and country is United States`() {
