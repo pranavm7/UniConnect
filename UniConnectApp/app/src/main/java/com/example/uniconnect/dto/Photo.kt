f0bf3fdbcefeb12c2935c5009985f406a6c11c93
Edward Bender <59852058+bendered2332@users.noreply.github.com>
2022-04-12 12:18:06 -0400

    added firebase storage to project and new photo dto class

---
+++ UniConnectApp/app/src/main/java/com/example/uniconnect/dto/Photo.kt
@@ -0,0 +1,6 @@
+package com.example.uniconnect.dto
+
+import java.util.*
+
+class Photo(var localUri: String = "", var remoteUri : String = "", var description: String = "", var dateTaken: Date = Date(), var id: String = "") {
+}
